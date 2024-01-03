9b6ff15006f6abeee5d1bdde21d84746357c99d1
Stefanni Brasil <stefannibrasil@gmail.com>
2024-01-03 08:11:58 -0700

    Render a React Component for Testing

---
+++ src/__tests__/favorite-number.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {FavoriteNumber} from '../favorite-number'
+
+test('renders a number input with a label "Favorite Number"', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<FavoriteNumber />, div)
+  // console.log(div.innerHTML)
+  expect(div.querySelector('input').type).toBe('number')
+  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+})
