de8ba96414aefa6e940ccfab2d486a8afad186dd
Stefanni Brasil <stefannibrasil@gmail.com>
2024-01-22 08:07:33 -0700

    Save post with the submit values

--- src/__tests__/tdd-01-markup.js
+++ src/__tests__/tdd-01-markup.js
@@ -1,19 +1,34 @@
 import * as React from 'react'
 import {fireEvent, render, screen} from '@testing-library/react'
 import {Editor} from '../post-editor-01-markup'
+import {savePost as mockSavePost} from '../api'
+
+jest.mock('../api')
 
 test('renders a form with title, content, tags, and a submit button', () => {
-  render(<Editor />)
-  screen.getByLabelText(/title/i)
-  screen.getByLabelText(/content/i)
-  screen.getByLabelText(/tags/i)
+  mockSavePost.mockResolvedValueOnce()
+  const fakeUser = {id: 'user-1'}
+  render(<Editor user={fakeUser} />)
+  const fakePost = {
+    title: 'Hello Cats',
+    content: 'This is about cats',
+    tags: ['cat1', 'cat2'],
+  }
+  screen.getByLabelText(/title/i).value = fakePost.title
+  screen.getByLabelText(/content/i).value = fakePost.content
+  screen.getByLabelText(/tags/i).value = fakePost.tags
   screen.getByText(/submit/i)
-
   const submitButton = screen.getByText(/submit/i)
 
   fireEvent.click(submitButton)
 
   expect(submitButton).toBeDisabled()
+
+  expect(mockSavePost).toHaveBeenCalledWith({
+    ...fakePost,
+    authorId: fakeUser.id,
+  })
+  expect(mockSavePost).toHaveBeenCalledTimes(1)
 })
 
 // disabling this rule for now. We'll get to this later
--- src/post-editor-01-markup.js
+++ src/post-editor-01-markup.js
@@ -1,22 +1,30 @@
 import * as React from 'react'
+import {savePost} from './api'
 
-function Editor() {
+function Editor({user}) {
   const [isSaving, setIsSaving] = React.useState(false)
   function handleSubmit(e) {
     e.preventDefault()
+    const {title, content, tags} = e.target.elements
     setIsSaving(true)
+    savePost({
+      title: title.value,
+      content: content.value,
+      tags: tags.value.split(',').map((t) => t.trim()),
+      authorId: user.id,
+    })
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <label htmlFor="title-input">Title</label>
-      <input id="title-input" />
+      <input id="title-input" name="title" />
 
       <label htmlFor="content-input">Content</label>
-      <textarea id="content-input" />
+      <textarea id="content-input" name="content" />
 
       <label htmlFor="tags-input">Tags</label>
-      <input id="tags-input" />
+      <input id="tags-input" name="tags" />
 
       <button type="submit" disabled={isSaving}>
         Submit
